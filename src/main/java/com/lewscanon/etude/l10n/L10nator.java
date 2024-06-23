69fd70b342378dc3c4b32263a04a0f14a1a4d30c
Lew Bloch <lewbloch@lewscanon.com>
2024-06-23 09:11:42 -0700

    L10nator.java looped with static imports.

--- src/main/java/com/lewscanon/etude/l10n/L10nator.java
+++ src/main/java/com/lewscanon/etude/l10n/L10nator.java
@@ -1,6 +1,4 @@
-/*
- * Copyright © 2024, Lewis S. Bloch. All rights reserved.
- */
+/* Copyright © 2024, Lewis S. Bloch. All rights reserved. */
 package com.lewscanon.etude.l10n;
 
 import java.time.ZoneId;
@@ -8,23 +6,31 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
+import java.util.Set;
 
 import static java.time.format.DateTimeFormatter.ofLocalizedDateTime;
+import static java.util.Locale.FRENCH;
+import static java.util.Locale.JAPANESE;
+import static java.util.Locale.UK;
+import static java.util.Locale.US;
 
 public class L10nator {
-    public static void main(String... args) {
-        final Locale defloc = Locale.getDefault();
-        final DateTimeFormatter defaultFormatter = ofLocalizedDateTime(FormatStyle.LONG).localizedBy(defloc);
-        final Locale french = Locale.FRANCE;
-        final DateTimeFormatter frenchFormatter = ofLocalizedDateTime(FormatStyle.LONG).localizedBy(french);
-        final Locale us = Locale.US;
-        final DateTimeFormatter usFormatter = ofLocalizedDateTime(FormatStyle.LONG).localizedBy(us);
+    record LocalFormatter(Locale locale, DateTimeFormatter formatter) {}
+    static final Set<LocalFormatter> localFormatters = Set.of(
+        new LocalFormatter(Locale.getDefault(), ofLocalizedDateTime(FormatStyle.LONG).localizedBy(Locale.getDefault())),
+        new LocalFormatter(FRENCH, ofLocalizedDateTime(FormatStyle.LONG).localizedBy(FRENCH)),
+        new LocalFormatter(JAPANESE, ofLocalizedDateTime(FormatStyle.LONG).localizedBy(JAPANESE)),
+        new LocalFormatter(UK, ofLocalizedDateTime(FormatStyle.LONG).localizedBy(UK)),
+        new LocalFormatter(US, ofLocalizedDateTime(FormatStyle.LONG).localizedBy(US))
+    );
 
+    public static void main(String... args) {
         final String SHOWIT = "Time per %s: %s\n";
 
         final ZonedDateTime rightNow = ZonedDateTime.now(ZoneId.of("Europe/Paris"));
-        System.out.printf(defloc, SHOWIT, defloc.getDisplayName(), rightNow.format(defaultFormatter));
-        System.out.printf(french, SHOWIT, french.getDisplayName(), rightNow.format(frenchFormatter));
-        System.out.printf(us, SHOWIT, us.getDisplayName(), rightNow.format(usFormatter));
+        for (final LocalFormatter formatter : localFormatters) {
+            System.out.printf(formatter.locale, SHOWIT, formatter.locale.getDisplayName(),
+                              rightNow.format(formatter.formatter));
+        }
     }
 }
