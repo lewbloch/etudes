91059ca7dbdaf341efe791e3ab1d372cd8119b61
Lew Bloch <lewbloch@lewscanon.com>
2024-07-09 12:41:40 -0700

    AndIQuote.java.

---
+++ src/main/java/com/lewscanon/etude/syntactics/AndIQuote.java
@@ -0,0 +1,13 @@
+/* Copyright © 2024, Lewis S. Bloch. All rights reserved. */
+package com.lewscanon.etude.syntactics;
+
+public class AndIQuote {
+    public static void main(String... args) {
+        System.out.println("Uncomment the comment to see what happens");
+
+        System.out.println();
+
+        // Uncomment the next line
+//         System.out.println(″This is not a String″);
+    }
+}
