1e8399d3e6cd9ef1e215a42d521a37426e5641c8
lewbloch <kw2kw@lewscanon.com>
2026-04-25 21:32:33 -0700

    given_sum_two_arrays.py.

---
+++ src/main/python/intervue/given_sum_two_arrays.py
@@ -0,0 +1,23 @@
+# Find pairs between two arrays that add to a given sum.
+# Copyright © 2026 Lewis S. Bloch.
+
+class Kount(dict[int, int]):
+    def __missing__(self, key):
+        return 0
+
+def pairs_that_sum(lefts, rights, target):
+    complems: Kount = Kount()
+
+    for left in lefts:
+        complems[left] += 1
+
+    for right in rights:
+        left = target - right
+        for x in range(0, complems[left]):
+            print(left, ' ', right, '\n')
+
+first = [17, -7, 0, 9, 3, 9]
+second = [16, -7, -6, 1, -1, 10, 1, -7, 9]
+targ = 10
+
+pairs_that_sum(first, second, targ)
