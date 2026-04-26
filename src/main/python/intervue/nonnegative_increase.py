50455f80145aa9337e978ccbce2c5c9531ac6f5c
lewbloch <kw2kw@lewscanon.com>
2026-04-26 16:57:15 -0700

    nonnegative_increase.py output format.

--- src/main/python/intervue/nonnegative_increase.py
+++ src/main/python/intervue/nonnegative_increase.py
@@ -4,18 +4,18 @@
 # 0 if no such non-negative difference exists,
 #
 # [16, 7, 6, 1, 1, 10, 1, 7, 9] => 9
+# [7, 6, 1, 16, 1, 10, 1, 7, 9] => 15
 # [] => 0
 # [12] => 0
 # [1, 1, 1, 6, 7, 7, 9, 10, 16] => 15
 # [16, 10, 9, 7, 7, 6, 1, 1, 1] => 0
-# [16, 10, 9, 7, 6, 1] => 0
 # [16, 7, 6, 1, -3, 10, 1, 7, 9] => -1
 # [-3, 10, 1, 7, 9] => -1
 #
 # Copyright © 2026 Lewis S. Bloch.
 
 def nonnegative_increase(values):
-    if len(values) == 0:
+    if not values:
         return 0
     increase = 0
     minim = values[0]
@@ -30,14 +30,14 @@ def nonnegative_increase(values):
 
 examples = (
     [16, 7, 6, 1, 1, 10, 1, 7, 9],
+    [7, 6, 1, 16, 1, 10, 1, 7, 9],
     [],
     [12],
     [1, 1, 1, 6, 7, 7, 9, 10, 16],
     [16, 10, 9, 7, 7, 6, 1, 1, 1],
-    [16, 10, 9, 7, 6, 1],
     [16, 7, 6, 1, -3, 10, 1, 7, 9],
     [-3, 10, 1, 7, 9],
 )
 
 for vals in examples:
-    print(nonnegative_increase(vals))
+    print(vals, "\n", nonnegative_increase(vals))
